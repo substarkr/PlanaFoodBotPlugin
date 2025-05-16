705da03fb7165ceecef7c92fbfa8bc7e41acb653
WINDCS <96674678+substarkr@users.noreply.github.com>
2025-05-16 21:03:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,13 +7,42 @@ food = [
     "豆浆油条",
     "鸡蛋灌饼",
     "皮蛋瘦肉粥",
-    "牛肉面",
+    "小笼包",
+    "煎饼果子",
+    "肠粉",
+    "牛奶燕麦",
+    "三明治",
+    "法式吐司",
+    "粢饭团",
     "黄焖鸡米饭",
+    "牛肉拉面",
     "麻辣香锅",
+    "盖浇饭",
+    "海南鸡饭",
+    "酸菜鱼",
+    "番茄牛腩饭",
+    "卤肉饭",
+    "咖喱鸡饭",
+    "排骨饭",
+    "火锅",
+    "烧烤",
     "寿司拼盘",
     "意大利面",
+    "披萨",
+    "牛排套餐",
+    "烤鱼",
+    "小龙虾",
+    "韩式炸鸡",
+    "日式拉面",
     "蔬菜沙拉",
-    "牛排套餐"
+    "鸡胸肉糙米饭",
+    "藜麦沙拉",
+    "素食套餐",
+    "低脂三明治",
+    "芒果班戟",
+    "提拉米苏",
+    "章鱼小丸子",
+    "鸡蛋仔"
 ]
 
 # 注册插件
@@ -35,30 +64,30 @@ class MyPlugin(BasePlugin):
         if msg == "早上吃什么":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug(f"普拉娜建议早上吃{random.choice(food)}")
+            self.ap.logger.debug(f"普拉娜建议早上吃{random.choice(food)}哦")
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", [f"普拉娜建议早上吃{random.choice(food)}"])
+            ctx.add_return("reply", [f"普拉娜建议早上吃{random.choice(food)}哦"])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
         elif msg == "中午吃什么":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug(f"普拉娜建议中午吃{random.choice(food)}")
+            self.ap.logger.debug(f"普拉娜建议中午吃{random.choice(food)}哦")
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", [f"普拉娜建议中午吃{random.choice(food)}"])
+            ctx.add_return("reply", [f"普拉娜建议中午吃{random.choice(food)}哦"])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
         elif msg == "晚上吃什么":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug(f"普拉娜建议晚上吃{random.choice(food)}")
+            self.ap.logger.debug(f"普拉娜建议晚上吃{random.choice(food)}哦")
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", [f"普拉娜建议晚上吃{random.choice(food)}"])
+            ctx.add_return("reply", [f"普拉娜建议晚上吃{random.choice(food)}哦"])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
@@ -70,30 +99,30 @@ class MyPlugin(BasePlugin):
         if msg == "早上吃什么":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug(f"普拉娜建议早上吃{random.choice(food)}")
+            self.ap.logger.debug(f"普拉娜建议早上吃{random.choice(food)}哦")
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", [f"普拉娜建议早上吃{random.choice(food)}"])
+            ctx.add_return("reply", [f"普拉娜建议早上吃{random.choice(food)}哦"])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
         elif msg == "中午吃什么":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug(f"普拉娜建议中午吃{random.choice(food)}")
+            self.ap.logger.debug(f"普拉娜建议中午吃{random.choice(food)}哦")
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", [f"普拉娜建议中午吃{random.choice(food)}"])
+            ctx.add_return("reply", [f"普拉娜建议中午吃{random.choice(food)}哦"])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
         elif msg == "晚上吃什么":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug(f"普拉娜建议晚上吃{random.choice(food)}")
+            self.ap.logger.debug(f"普拉娜建议晚上吃{random.choice(food)}哦")
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", [f"普拉娜建议晚上吃{random.choice(food)}"])
+            ctx.add_return("reply", [f"普拉娜建议晚上吃{random.choice(food)}哦"])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
